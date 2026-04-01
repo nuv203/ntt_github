1e102cede5e145df2f340e72b6b06afc27aad949
Niles <nuv203@nyu.edu>
2026-03-31 21:04:11 -0400

    added platform directory

---
+++ kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/libsrc/standalone/src/microblaze_interrupt_handler.d
@@ -0,0 +1,18 @@
+/home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/libsrc/standalone/src/microblaze_interrupt_handler.o: \
+ /home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/standalone/src/microblaze/microblaze_interrupt_handler.c \
+ /home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/standalone/src/microblaze/xil_exception.h \
+ /home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/include/xil_types.h \
+ /home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/include/bspconfig.h \
+ /home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/include/xmem_config.h \
+ /home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/include/xparameters.h \
+ /home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/standalone/src/microblaze/microblaze_interrupts_i.h \
+ /home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/include/xil_assert.h \
+ /home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/include/xil_types.h
+/home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/standalone/src/microblaze/xil_exception.h:
+/home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/include/xil_types.h:
+/home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/include/bspconfig.h:
+/home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/include/xmem_config.h:
+/home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/include/xparameters.h:
+/home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/standalone/src/microblaze/microblaze_interrupts_i.h:
+/home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/include/xil_assert.h:
+/home/nuv203/ssd/kria_xrt_testing/kria_plat/zynqmp_pmufw/zynqmp_pmufw_bsp/libsrc/build_configs/gen_bsp/include/xil_types.h:
